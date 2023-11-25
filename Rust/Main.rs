43a0ba6c27132427436437b4bcbf69eb46c3a932
VimEnthusiast <139187584+VimEnthusiast@users.noreply.github.com>
2023-11-25 08:16:48 -0500

    Update Main.rs

--- Rust/Main.rs
+++ Rust/Main.rs
@@ -1,7 +1,21 @@
 fn main() {
     println!("Hello, world!");
-    let_Hello = 89
-    let_World = 234
-    let_HelloWorld = Hello*World
-    println!(HelloWorld)
+    let hello = 89;
+    let world = 234;
+    let hello_world = hello * world;
+    println!("{}", hello_world);
+
+    let mut pi = 0.0;
+    let mut positive = true;
+    for i in 0..1_000_000 {
+        let term = 1.0 / (i as f64 * 2.0 + 1.0);
+        if positive {
+            pi += term;
+        } else {
+            pi -= term;
+        }
+        positive = !positive;
+    }
+    pi *= 4.0;
+    println!("Approximation of Pi: {}", pi);
 }
